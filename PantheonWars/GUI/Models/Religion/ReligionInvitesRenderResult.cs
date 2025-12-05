1edc63cd9ce2a646290fe89b75064a70c0f26ac2
quantumheart <cody.mumford9106@hotmail.com>
2025-12-05 07:58:26 -0600

    refactor: add models for state -> VM -> renderer -> events -> state

---
+++ PantheonWars/GUI/Models/Religion/ReligionInvitesRenderResult.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using PantheonWars.GUI.Events;
+
+namespace PantheonWars.GUI.Models.Religion;
+
+public readonly struct ReligionInvitesRenderResult(
+    IReadOnlyList<ReligionInvitesEvent> events,
+    float renderedHeight)
+{
+    public IReadOnlyList<ReligionInvitesEvent> Events { get; } = events;
+    public float RenderedHeight { get; } = renderedHeight;
+}
