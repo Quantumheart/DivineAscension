08b07eced4dbc1c61ab657d31b6d5b896fd61bc9
quantumheart <cody.mumford9106@hotmail.com>
2025-12-29 13:40:08 -0600

    chore: update modinfo.json

--- DivineAscension/Properties/AssemblyInfo.cs
+++ DivineAscension/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using Vintagestory.API.Common;
 [assembly: ModInfo("Divine Ascension", "divineascension",
     Description =
         "A religious-themed PvP mod featuring competing deities with unique abilities and favor-based progression system.",
-    Version = "1.17.1",
+    Version = "1.17.2",
     Authors = new[] { "Valinnar" },
     Side = "Universal")]
 
