64b980251256d80aa22e3fe870d4c70993d76ea4
Quantumheart <32171900+Quantumheart@users.noreply.github.com>
2026-01-22 18:39:12 +0000

    chore(release): bump version to 4.19.0

--- DivineAscension/Properties/AssemblyInfo.cs
+++ DivineAscension/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using Vintagestory.API.Common;
 [assembly: ModInfo("Divine Ascension", "divineascension",
     Description =
         "A religious-themed PvP mod featuring competing deities with unique abilities and favor-based progression system.",
-    Version = "4.18.0",
+    Version = "4.19.0",
     Authors = new[] { "Valinnar" },
     Side = "Universal")]
 
